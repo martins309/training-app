1c5124a92c8eb2d410c64c4dccb622c547b72c7f
Andrew Martins Jr. <75143566+martins309@users.noreply.github.com>
2024-01-03 05:24:20 +0000

    added the dashboard component

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      
-    </NavigationContainer>
-    
+    <View>
+      <Text>This is the navigation portion of the app</Text>
+    </View>
   );
+}
+
+const Stack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigtaion>
+        <Stack.Screen
+          name= 'HomePage'
+        />
+      </Stack.Navigtaion>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
---
+++ components/DashBoard.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Text, View } from 'react-native'
+
+
+export const DashBoard = () => {
+    return (
+        <View>
+            <Text>this is the dashboard</Text>
+        </View>
+    )
+}
+
